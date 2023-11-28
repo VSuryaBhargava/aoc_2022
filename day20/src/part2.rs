5694776a819890d96d05ebda7b799dfbb4920800
Surya Bhargavav Vemuri <surya@relicx.ai>
2023-11-29 04:00:54 +0530

    Use part1 of day20 in part2

--- day20/src/part2.rs
+++ day20/src/part2.rs
@@ -1,10 +1,10 @@
-use utils::problem::SolvePart2;
 
 use crate::{parser::ParsedOutput, Problem};
+use utils::problem::{SolvePart1, SolvePart2};
 
 impl SolvePart2 for Problem {
     type ParsedType = ParsedOutput;
     fn solve_part_two(&mut self, parsed_input: ParsedOutput) -> String {
-        crate::part1::solve_part_one(self, parsed_input)
+        self.solve_part_one(parsed_input)
     }
 }
