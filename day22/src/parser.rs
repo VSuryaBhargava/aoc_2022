b3750b428ccde90c293cb8af54ce9f3dbb86e3e1
Surya Bhargavav Vemuri <surya@relicx.ai>
2023-12-01 09:59:10 +0530

    Parse Day 22

--- day22/src/parser.rs
+++ day22/src/parser.rs
@@ -1,9 +1,19 @@
 use utils::{
-    nom::combinator::{all_consuming, map},
-    parser::{parse_number_i32, ParseInput},
+    nom::{
+        branch::alt,
+        character::complete::char,
+        combinator::{map, opt},
+        multi::many1,
+        IResult,
+    },
+    parser::{parse_number_u32, ParseInput},
 };
 
-pub type ParsedOutput = Vec<i32>;
+#[derive(Debug)]
+pub struct ParsedOutput {
+    pub instructions: Vec<Instructions>,
+    pub map: Vec<Vec<Points>>,
+}
 
 #[derive(Default)]
 pub struct Problem {}
@@ -11,14 +21,66 @@ pub struct Problem {}
 impl ParseInput for Problem {
     type ParsedType = ParsedOutput;
     fn parse(&mut self, input: String, _: bool) -> ParsedOutput {
-        let mut parsed_input: Vec<i32> = vec![];
+        let mut parsed_input = ParsedOutput {
+            instructions: Vec::new(),
+            map: Vec::new(),
+        };
 
         input.lines().for_each(|l| {
-            let _ = map(all_consuming(parse_number_i32), |data| {
-                parsed_input.push(data);
+            let _ = map(parse_line, |(ins, points)| {
+                if let Some(ins) = ins {
+                    parsed_input.instructions = ins;
+                }
+                if let Some(points) = points {
+                    parsed_input.map.push(points);
+                }
             })(l);
         });
 
         parsed_input
     }
+}
+
+#[derive(Debug)]
+pub enum Instructions {
+    Move(u32),
+    Right,
+    Left,
+}
+
+#[derive(Debug)]
+pub enum Points {
+    OutOfBounds,
+    Wall,
+    Empty,
+}
+
+fn parse_map(input: &str) -> IResult<&str, Vec<Points>> {
+    many1(alt((
+        map(char(' '), |_| Points::OutOfBounds),
+        map(char('.'), |_| Points::Empty),
+        map(char('#'), |_| Points::Wall),
+    )))(input)
+}
+
+fn parse_instructions(input: &str) -> IResult<&str, Vec<Instructions>> {
+    many1(alt((
+        map(parse_number_u32, Instructions::Move),
+        map(char('L'), |_| Instructions::Left),
+        map(char('R'), |_| Instructions::Right),
+    )))(input)
+}
+
+fn parse_line(input: &str) -> IResult<&str, (Option<Vec<Instructions>>, Option<Vec<Points>>)> {
+    let (input, instructions) = opt(parse_instructions)(input)?;
+    if let Some(instructions) = instructions {
+        return Ok((input, (Some(instructions), None)));
+    }
+
+    let (input, points) = opt(parse_map)(input)?;
+    if let Some(points) = points {
+        return Ok((input, (None, Some(points))));
+    }
+
+    Ok((input, (None, None)))
 }
--- day22/src/part1.rs
+++ day22/src/part1.rs
@@ -5,7 +5,8 @@ use crate::parser::{ParsedOutput, Problem};
 impl SolvePart1 for Problem {
     type ParsedType = ParsedOutput;
 
-    fn solve_part_one(&mut self, _parsed_input: ParsedOutput) -> String {
+    fn solve_part_one(&mut self, parsed_input: ParsedOutput) -> String {
+        println!("{:?}", parsed_input);
         todo!("Implement part one");
     }
 }
